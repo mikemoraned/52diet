00c4cd7d89e86784da3de2154665ed05b89bfbea
mikemoraned <mike@houseofmoran.com>
2013-07-29 20:56:58 +0100

    map to line chart, based on http://bl.ocks.org/mbostock/3883245 example

--- js/main.js
+++ js/main.js
@@ -1,15 +1,70 @@
 $(function() {
     console.log("BEGINETH THE HACKING");
-    d3.json("/js/data/snapshot.json", function(data) {
-       console.dir(data);
-       // Mon, 29 Jul 2013 00:00:00
-       var items = data.items;
-       var weightOverTime = items.map(function(d) {
-           return {
-               'weight' : d.weight,
-               'time'   : moment(d.timestamp, "ddd, DD MMM YYYY hh:mm:ss").toDate()
-           };
-       });
-       console.dir(weightOverTime);
+
+    function toWeightOverTime(data) {
+        var items = data.items;
+        return items.map(function(d) {
+            return {
+                'weight' : d.weight,
+                'date'   : moment(d.timestamp, "ddd, DD MMM YYYY hh:mm:ss").toDate()
+            };
+        });
+    }
+
+    var margin = {top: 20, right: 20, bottom: 30, left: 50},
+        width = 960 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
+
+    var x = d3.time.scale()
+        .range([0, width]);
+
+    var y = d3.scale.linear()
+        .range([height, 0]);
+
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left");
+
+    var line = d3.svg.line()
+        .x(function(d) { return x(d.date); })
+        .y(function(d) { return y(d.weight); });
+
+    var svg = d3.select("svg#chart")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    d3.json("/js/data/snapshot.json", function(json) {
+        var data = toWeightOverTime(json);
+
+        console.dir(data);
+
+        x.domain(d3.extent(data, function(d) { return d.date; }));
+        y.domain(d3.extent(data, function(d) { return d.weight; }));
+
+        svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis);
+
+        svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis)
+            .append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 6)
+            .attr("dy", ".71em")
+            .style("text-anchor", "end")
+            .text("Price ($)");
+
+        svg.append("path")
+            .datum(data)
+            .attr("class", "line")
+            .attr("d", line);
     });
 });
