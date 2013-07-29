1930cf1b26c899a4b8eab1e6daeb6b5d6dc5974c
mikemoraned <mike@houseofmoran.com>
2013-07-30 00:11:55 +0100

    add version of chart created via dc and crossfilter; note that had to hack max to handle case when there are multiple values for a day

--- js/main.js
+++ js/main.js
@@ -43,7 +43,7 @@ $(function() {
     d3.json("/js/data/snapshot.json", function(json) {
         var data = toWeightOverTime(json);
 
-        console.dir(data);
+        //console.dir(data);
 
         x.domain(d3.extent(data, function(d) { return d.date; }));
         y.domain(d3.extent(data, function(d) { return d.weight; }));
@@ -67,5 +67,58 @@ $(function() {
             .datum(data)
             .attr("class", "line")
             .attr("d", line);
+
+        var cross = crossfilter(data);
+
+        var all = cross.groupAll();
+        //console.log(all);
+
+        function reduceByMax(group, fn) {
+            function reduceAdd(p, v) {
+                if (fn(v) > p) {
+                    p = fn(v);
+                }
+                return p;
+            }
+
+            function reduceRemove(p, v) {
+                throw "not implemented";
+            }
+
+            function reduceInitial() {
+                return 0;
+            }
+
+            return group.reduce(reduceAdd, reduceRemove, reduceInitial);
+        }
+
+        var dayDimension = cross.dimension(function(d) { return d.date; });
+        var weightDimension = cross.dimension(function(d) { return d.weight; });
+        weightDimension.filterRange([80, 100]);
+        var weightByDayGroup = reduceByMax(dayDimension.group(), function(d) { return d.weight; });
+
+        console.dir(dayDimension.top(10));
+        console.dir(weightByDayGroup.top(10));
+
+        var minDay = dayDimension.top(1)[0].date;
+        var maxDay = dayDimension.bottom(1)[0].date;
+
+        var minWeight = weightDimension.bottom(1)[0].weight;
+        var maxWeight = weightDimension.top(1)[0].weight;
+
+        console.dir(minDay);
+        console.dir(maxDay);
+
+        dc.lineChart("#weight-by-day-chart")
+            .width(990)
+            .height(250)
+            .dimension(dayDimension)
+            .group(weightByDayGroup)
+            .margins({top: 10, right: 50, bottom: 30, left: 40})
+            .x(d3.time.scale().domain([maxDay, minDay]))
+            .y(d3.scale.linear().domain([minWeight, maxWeight]))
+            ;
+
+        dc.renderAll();
     });
 });
