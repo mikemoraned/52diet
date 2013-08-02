b045ecea448489cf5223c21781867344fcb89d90
mikemoraned <mike@houseofmoran.com>
2013-08-02 16:15:13 +0200

    starting to muck about with two.js

--- js/main.js
+++ js/main.js
@@ -12,7 +12,30 @@ $(function() {
         return values;
     }
 
-    d3.json("/js/data/snapshot.json", function(json) {
+    function renderSpiral() {
+        // Make an instance of two and place it on the page.
+        var elem = document.getElementById('weight-by-day-spiral-chart');
+        var two = new Two({ width: 285, height: 200 }).appendTo(elem);
+
+        var circle = two.makeCircle(-70, 0, 50);
+        var rect = two.makeRectangle(70, 0, 100, 100);
+        circle.fill = '#FF8000';
+        circle.stroke = 'orangered';
+        rect.fill = 'rgba(0, 200, 255, 0.75)';
+        rect.stroke = '#1C75BC';
+
+// Groups can take an array of shapes and/or groups.
+        var group = two.makeGroup(circle, rect);
+
+// And have translation, rotation, scale like all shapes.
+        group.translation.set(two.width / 2, two.height / 2);
+        group.rotation = Math.PI / 2;
+        group.scale = 0.75;
+
+        two.update();
+    }
+
+    function renderChart(json) {
         var data = toWeightOverTime(json);
 
         var cross = crossfilter(data);
@@ -63,8 +86,13 @@ $(function() {
             .margins({top: 10, right: 50, bottom: 30, left: 40})
             .x(d3.time.scale().domain([maxDay, minDay]))
             .y(d3.scale.linear().domain([minWeight, maxWeight]))
-            ;
+        ;
 
         dc.renderAll();
+    }
+
+    d3.json("/js/data/snapshot.json", function(json) {
+        //renderChart(json);
+        renderSpiral();
     });
 });
