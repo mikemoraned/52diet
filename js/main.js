b972bd28494657e71c30f8db1e54c36996473dd7
mikemoraned <mike@houseofmoran.com>
2013-08-02 16:25:30 +0200

    animation

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ $(function() {
     function renderSpiral() {
         // Make an instance of two and place it on the page.
         var elem = document.getElementById('weight-by-day-spiral-chart');
-        var two = new Two({ width: 285, height: 200 }).appendTo(elem);
+        var two = new Two({ width: 400, height: 400 }).appendTo(elem);
 
         var circle = two.makeCircle(-70, 0, 50);
         var rect = two.makeRectangle(70, 0, 100, 100);
@@ -32,7 +32,19 @@ $(function() {
         group.rotation = Math.PI / 2;
         group.scale = 0.75;
 
-        two.update();
+
+// Bind a function to scale and rotate the group
+// to the animation loop.
+        two.bind('update', function(frameCount) {
+            // This code is called everytime two.update() is called.
+            // Effectively 60 times per second.
+            if (group.scale > 0.9999) {
+                group.scale = group.rotation = 0;
+            }
+            var t = (1 - group.scale) * 0.125;
+            group.scale += t;
+            group.rotation += t * 4 * Math.PI;
+        }).play();
     }
 
     function renderChart(json) {
